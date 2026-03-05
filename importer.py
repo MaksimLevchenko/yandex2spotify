37710da4c145262b7af3202b53685704402b9d9e
EliteNoobest <Valvekat@mail.ru>
2026-03-05 22:47:57 +0300

    Implement progress saving and loading for Spotify imports

--- importer.py
+++ importer.py
@@ -1,6 +1,7 @@
 import json
 import argparse
 import logging
+import os
 from base64 import b64encode
 from time import sleep
 
@@ -13,6 +14,10 @@ from yandex_music import Client, Artist
 
 REDIRECT_URI = 'https://open.spotify.com'
 MAX_REQUEST_RETRIES = 5
+PROGRESS_FILE = 'progress.json'
+FLUSH_SPOTIFY_BATCH = 20
+FLUSH_SPOTIFY_LIBRARY_BATCH = 10
+PROGRESS_FLUSH_EVERY = 10
 
 logging.basicConfig(
     level=logging.INFO,
@@ -87,6 +92,63 @@ class Importer:
         logger.info(f'User ID: {self.user}')
 
         self.not_imported = {}
+        self._progress = self._load_progress()
+        self._progress_dirty = 0
+
+    def _load_progress(self):
+        if not os.path.exists(PROGRESS_FILE):
+            return {
+                'likes': {'index': 0, 'buffer': []},
+                'albums': {'index': 0, 'buffer': []},
+                'artists': {'index': 0, 'buffer': []},
+                'playlists': {}
+            }
+        try:
+            with open(PROGRESS_FILE, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+            if 'likes' not in data:
+                data['likes'] = {'index': 0, 'buffer': []}
+            if 'albums' not in data:
+                data['albums'] = {'index': 0, 'buffer': []}
+            if 'artists' not in data:
+                data['artists'] = {'index': 0, 'buffer': []}
+            if 'playlists' not in data or not isinstance(data['playlists'], dict):
+                data['playlists'] = {}
+
+            for k in ['likes', 'albums', 'artists']:
+                if 'index' not in data[k]:
+                    data[k]['index'] = 0
+                if 'buffer' not in data[k] or not isinstance(data[k]['buffer'], list):
+                    data[k]['buffer'] = []
+
+            for pk, pv in list(data['playlists'].items()):
+                if not isinstance(pv, dict):
+                    data['playlists'][pk] = {'index': 0, 'buffer': []}
+                else:
+                    if 'index' not in pv:
+                        pv['index'] = 0
+                    if 'buffer' not in pv or not isinstance(pv['buffer'], list):
+                        pv['buffer'] = []
+
+            return data
+        except Exception:
+            return {
+                'likes': {'index': 0, 'buffer': []},
+                'albums': {'index': 0, 'buffer': []},
+                'artists': {'index': 0, 'buffer': []},
+                'playlists': {}
+            }
+
+    def _save_progress(self, force=False):
+        if not force and self._progress_dirty < PROGRESS_FLUSH_EVERY:
+            return
+        with open(PROGRESS_FILE, 'w', encoding='utf-8') as f:
+            json.dump(self._progress, f, ensure_ascii=False, indent=2)
+        self._progress_dirty = 0
+
+    def _bump_progress_dirty(self):
+        self._progress_dirty += 1
+        self._save_progress(force=False)
 
     def _import_item(self, item):
         # if the item is a string, it is a query from the JSON file
@@ -123,8 +185,42 @@ class Importer:
 
         return found_items[0]['id']
 
-    def _add_items_to_spotify(self, items, not_imported_section, save_items_callback):
+    def _flush_buffer(self, buffer_ids, save_items_callback):
+        if not buffer_ids:
+            return
+        for chunk in chunks(buffer_ids, FLUSH_SPOTIFY_BATCH):
+            save_items_callback(self, chunk)
+        buffer_ids.clear()
+
+    def _save_uris_to_library(self, uris):
+        for uri_chunk in chunks(uris, FLUSH_SPOTIFY_LIBRARY_BATCH):
+            handle_spotify_exception(self.spotify_client._put)(
+                'me/library',
+                args={'uris': ','.join(uri_chunk)}
+            )
+
+    def _create_playlist(self, playlist_name):
+        return handle_spotify_exception(self.spotify_client._post)(
+            'me/playlists',
+            payload={'name': playlist_name, 'public': True}
+        )
+
+    def _add_uris_to_playlist(self, playlist_id, uris):
+        return handle_spotify_exception(self.spotify_client._post)(
+            f'playlists/{playlist_id}/items',
+            payload={'uris': uris}
+        )
+
+    def _add_items_to_spotify(self, items, not_imported_section, save_items_callback, progress_node):
         spotify_items = []
+
+        existing_buffer = progress_node.get('buffer', [])
+        if existing_buffer:
+            spotify_items.extend(existing_buffer)
+            progress_node['buffer'] = []
+            self._save_progress(force=True)
+            self._flush_buffer(spotify_items, save_items_callback)
+            self._save_progress(force=True)
 
         items.reverse()
         for item in items:
@@ -132,6 +228,8 @@ class Importer:
                 spotify_id = self._import_item(item)
                 if spotify_id is None:
                     logger.warning('Item ID is None, skipping...')
+                    progress_node['index'] = int(progress_node.get('index', 0)) + 1
+                    self._bump_progress_dirty()
                     continue
                 spotify_items.append(spotify_id)
                 logger.info('OK')
@@ -140,17 +238,29 @@ class Importer:
                 not_imported_section.append(exception.item_name)
                 logger.warning('NO')
             except SpotifyException:
-                not_imported_section.append(item.title)
+                not_imported_section.append(item.title if not isinstance(item, str) else item)
                 logger.warning('NO')
 
-        if not spotify_items:
-            logger.info('No valid Spotify items to add.')
-            return
+            progress_node['index'] = int(progress_node.get('index', 0)) + 1
+            if spotify_items:
+                progress_node['buffer'] = list(spotify_items)
+            self._bump_progress_dirty()
 
-        for chunk in chunks(spotify_items, 50):
-            save_items_callback(self, chunk)
+            if len(spotify_items) >= FLUSH_SPOTIFY_BATCH:
+                progress_node['buffer'] = list(spotify_items)
+                self._save_progress(force=True)
+                self._flush_buffer(spotify_items, save_items_callback)
+                progress_node['buffer'] = []
+                self._save_progress(force=True)
 
+        if spotify_items:
+            progress_node['buffer'] = list(spotify_items)
+            self._save_progress(force=True)
+            self._flush_buffer(spotify_items, save_items_callback)
+            progress_node['buffer'] = []
+            self._save_progress(force=True)
 
+        self._save_progress(force=True)
 
     def import_likes(self):
         self.not_imported['Likes'] = []
@@ -159,17 +269,30 @@ class Importer:
         tracks = self.yandex_client.tracks([f'{track.id}:{track.album_id}' for track in likes_tracks if track.album_id])
         logger.info('Importing liked tracks...')
 
+        total = len(tracks)
+        node = self._progress['likes']
+        start_index = int(node.get('index', 0))
+        if start_index < 0:
+            start_index = 0
+        if start_index > total:
+            start_index = total
+
+        if start_index > 0:
+            logger.info(f'Resuming liked tracks from index {start_index}/{total}...')
+
+        tracks = tracks[start_index:]
+
         def save_tracks_callback(importer, spotify_tracks):
             logger.info(f'Saving {len(spotify_tracks)} tracks...')
-            handle_spotify_exception(importer.spotify_client.current_user_saved_tracks_add)(spotify_tracks)
+            importer._save_uris_to_library([f'spotify:track:{track_id}' for track_id in spotify_tracks])
             logger.info('OK')
 
-        self._add_items_to_spotify(tracks, self.not_imported['Likes'], save_tracks_callback)
+        self._add_items_to_spotify(tracks, self.not_imported['Likes'], save_tracks_callback, node)
 
     def import_playlists(self):
         playlists = self.yandex_client.users_playlists_list()
         for playlist in playlists:
-            spotify_playlist = handle_spotify_exception(self.spotify_client.user_playlist_create)(self.user, playlist.title)
+            spotify_playlist = self._create_playlist(playlist.title)
             spotify_playlist_id = spotify_playlist['id']
 
             logger.info(f'Importing playlist {playlist.title}...')
@@ -190,14 +313,29 @@ class Importer:
             else:
                 tracks = []
 
+            total = len(tracks)
+            key = str(playlist.kind)
+            if key not in self._progress['playlists']:
+                self._progress['playlists'][key] = {'index': 0, 'buffer': []}
+
+            node = self._progress['playlists'][key]
+            start_index = int(node.get('index', 0))
+            if start_index < 0:
+                start_index = 0
+            if start_index > total:
+                start_index = total
+
+            if start_index > 0:
+                logger.info(f'Resuming playlist {playlist.title} from index {start_index}/{total}...')
+
+            tracks = tracks[start_index:]
+
             def save_tracks_callback(importer, spotify_tracks):
                 logger.info(f'Saving {len(spotify_tracks)} tracks in playlist {playlist.title}...')
-                handle_spotify_exception(importer.spotify_client.user_playlist_add_tracks)(importer.user,
-                                                                                           spotify_playlist_id,
-                                                                                           spotify_tracks)
+                importer._add_uris_to_playlist(spotify_playlist_id, [f'spotify:track:{track_id}' for track_id in spotify_tracks])
                 logger.info('OK')
 
-            self._add_items_to_spotify(tracks, self.not_imported[playlist.title], save_tracks_callback)
+            self._add_items_to_spotify(tracks, self.not_imported[playlist.title], save_tracks_callback, node)
 
     def import_albums(self):
         self.not_imported['Albums'] = []
@@ -206,12 +344,25 @@ class Importer:
         albums = [album.album for album in likes_albums]
         logger.info('Importing albums...')
 
+        total = len(albums)
+        node = self._progress['albums']
+        start_index = int(node.get('index', 0))
+        if start_index < 0:
+            start_index = 0
+        if start_index > total:
+            start_index = total
+
+        if start_index > 0:
+            logger.info(f'Resuming albums from index {start_index}/{total}...')
+
+        albums = albums[start_index:]
+
         def save_albums_callback(importer, spotify_albums):
             logger.info(f'Saving {len(spotify_albums)} albums...')
-            handle_spotify_exception(importer.spotify_client.current_user_saved_albums_add)(spotify_albums)
+            importer._save_uris_to_library([f'spotify:album:{album_id}' for album_id in spotify_albums])
             logger.info('OK')
 
-        self._add_items_to_spotify(albums, self.not_imported['Albums'], save_albums_callback)
+        self._add_items_to_spotify(albums, self.not_imported['Albums'], save_albums_callback, node)
 
     def import_artists(self):
         self.not_imported['Artists'] = []
@@ -220,16 +371,35 @@ class Importer:
         artists = [artist.artist for artist in likes_artists]
         logger.info('Importing artists...')
 
+        total = len(artists)
+        node = self._progress['artists']
+        start_index = int(node.get('index', 0))
+        if start_index < 0:
+            start_index = 0
+        if start_index > total:
+            start_index = total
+
+        if start_index > 0:
+            logger.info(f'Resuming artists from index {start_index}/{total}...')
+
+        artists = artists[start_index:]
+
         def save_artists_callback(importer, spotify_artists):
             logger.info(f'Saving {len(spotify_artists)} artists...')
-            handle_spotify_exception(importer.spotify_client.user_follow_artists)(spotify_artists)
+            importer._save_uris_to_library([f'spotify:artist:{artist_id}' for artist_id in spotify_artists])
             logger.info('OK')
 
-        self._add_items_to_spotify(artists, self.not_imported['Artists'], save_artists_callback)
+        self._add_items_to_spotify(artists, self.not_imported['Artists'], save_artists_callback, node)
 
     def import_all(self):
-        for item in self._importing_items.values():
-            item()
+        try:
+            for item in self._importing_items.values():
+                item()
+        finally:
+            try:
+                self._save_progress(force=True)
+            except Exception:
+                pass
 
         self.print_not_imported()
 
@@ -261,15 +431,21 @@ class Importer:
                 not_imported.append(query)
                 logger.warning('NO')
 
-        # Create a new playlist
-        playlist_name = 'Imported from JSON'
-        playlist = handle_spotify_exception(self.spotify_client.user_playlist_create)(self.user, playlist_name)
+            if len(spotify_tracks) >= FLUSH_SPOTIFY_BATCH:
+                for chunk in chunks(spotify_tracks, FLUSH_SPOTIFY_BATCH):
+                    logger.info(f'Saving {len(chunk)} tracks...')
+                    self._save_uris_to_library([f'spotify:track:{track_id}' for track_id in chunk])
+                    logger.info('OK')
+                spotify_tracks.clear()
 
-        # Add tracks to the new playlist
-        for chunk in chunks(spotify_tracks, 50):
-            logger.info(f'Saving {len(chunk)} tracks...')
-            handle_spotify_exception(self.spotify_client.user_playlist_add_tracks)(self.user, playlist['id'], chunk)
-            logger.info('OK')
+        playlist_name = 'Imported from JSON'
+        playlist = self._create_playlist(playlist_name)
+
+        if spotify_tracks:
+            for chunk in chunks(spotify_tracks, FLUSH_SPOTIFY_BATCH):
+                logger.info(f'Saving {len(chunk)} tracks...')
+                self._add_uris_to_playlist(playlist['id'], [f'spotify:track:{track_id}' for track_id in chunk])
+                logger.info('OK')
 
         logger.error('Not imported tracks:')
         for track in not_imported:
@@ -302,7 +478,7 @@ if __name__ == '__main__':
             client_id=arguments.id,
             client_secret=arguments.secret,
             redirect_uri=REDIRECT_URI,
-            scope='playlist-modify-public, user-library-modify, user-follow-modify, ugc-image-upload',
+            scope='playlist-modify-public user-library-modify user-follow-modify ugc-image-upload',
             username=arguments.spotify,
         )
 
@@ -322,5 +498,10 @@ if __name__ == '__main__':
             importer_instance.import_from_json(arguments.json_path)
         else:
             importer_instance.import_all()
+    except KeyboardInterrupt:
+        try:
+            logger.error('Interrupted.')
+        except Exception:
+            pass
     except Exception as e:
         logger.error(f'An unexpected error occurred: {str(e)}')
